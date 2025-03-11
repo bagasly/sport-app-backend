c11a3697f7204ceb024e850b89992c3ed766da16
unknown <yasidalmubarok@gmail.com>
2025-03-11 13:57:12 +0700

    Modified: Redis and Model in prisma

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/auth.controller");
-
-router.post("/register", authController.register);
-router.post("/verify-otp", authController.verifyOtpAndCreateUser);
-
-module.exports = router;
