57fbcc922931e3e472178d7f41713a878a0867af
unknown <yasidalmubarok@gmail.com>
2025-03-13 19:50:23 +0700

    Modified: minor modify

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth.routes");
+const protectedRoutes = require("./routes/protected.routes");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
+app.use("/api/protected", protectedRoutes);
 app.use("/api/auth", authRoutes);
 
 module.exports = app;
--- src/controllers/users/auth.controller.js
+++ src/controllers/users/auth.controller.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("../../utils/users/jwt");
 const { sendOtp } = require("../../utils/users/generateOtp");
 const { storeOtp, verifyOtp, deleteOtp } = require("../../controllers/users/otp.service");
+const e = require("express");
 
 const prisma = new PrismaClient();
 
@@ -19,9 +20,16 @@ exports.register = async (req, res) => {
       return res.status(400).json({ message: "Email tidak valid" });
     }
 
-    const phoneRegex = /^\d{10,15}$/;
+    const phoneRegex = /^(08|62)\d{10,14}$/;
+
     if (!phoneRegex.test(phone)) {
       return res.status(400).json({ message: "Nomor ponsel tidak valid" });
+    }
+    
+    const passwordRegex = /^.{8,}$/;
+
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({ message: "Minimal 8 karakter" });
     }
 
     if (password !== confirmPassword) {
@@ -29,7 +37,7 @@ exports.register = async (req, res) => {
     }
 
     const existingUser = await prisma.user.findUnique({ where: { email } });
-    if (existingUser) return res.status(400).json({ message: "Email sudah terdaftar" });
+    if (existingUser) return res.status(409).json({ message: "Email telah terdaftar" });
 
     // Simpan OTP ke Redis
     const otpCode = await storeOtp(phone);
@@ -58,7 +66,7 @@ exports.verifyOtpAndCreateUser = async (req, res) => {
         return res.status(400).json({ message: "OTP tidak valid atau kadaluarsa" });
       }
   
-      const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await bcrypt.hash(password, process.env.SALT_ROUNDS);
   
       // Simpan user ke database
       const user = await prisma.user.create({
@@ -69,7 +77,7 @@ exports.verifyOtpAndCreateUser = async (req, res) => {
       await deleteOtp(phone);
   
       // Generate JWT token
-      const token = jwt.generateToken({ id: user.id });
+      const token = jwt.generateToken({ id: user.id, fullname: user.fullname, email: user.email, role: user.role });
   
       return res.status(201).json({ message: "Registrasi berhasil", token });
     } catch (error) {
@@ -82,13 +90,13 @@ exports.login = async (req, res) => {
       const { username, password } = req.body;
   
       const user = await prisma.user.findUnique({ where: { username } });
-      if (!user) return res.status(404).json({ message: "Invalid username or password" });
+      if (!user) return res.status(404).json({ message: "username atau password salah" });
   
       const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) return res.status(401).json({ message: "Invalid username or password" });
+      if (!isPasswordValid) return res.status(401).json({ message: "username atau password salah" });
   
       // Generate JWT token
-      const token = jwt.generateToken({ id: user.id });
+      const token = jwt.generateToken({ id: user.id, fullname: user.fullname, email: user.email, role: user.role });
   
       return res.status(200).json({ message: "Login berhasil", token });
     } catch (error) {
